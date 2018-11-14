287069182c23dbe2035e354d33422779cf2fcd00
unknown <hany-rashwan.softenghanyr@gmail.com>
2018-11-14 02:36:06 +0200

    update deploy

---
+++ mobile-config.js
@@ -0,0 +1,5 @@
+//App.setPreference('WebAppStartupTimeout', 30000);
+App.accessRule('*');
+//App.accessRule('https://mohe.herokuapp.com', { type: 'navigation' });
+//App.accessRule('data:*', { type: 'navigation' });
+//App.setPreference('android-targetSdkVersion', '22');
